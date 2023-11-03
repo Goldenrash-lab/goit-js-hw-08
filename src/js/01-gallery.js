97ffd6469b34b74cae3f0e67b981b3d7876f3717
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-11-03 20:08:41 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
+const listEl = document.querySelector('.gallery');
+
+function imageItems(items) {
+  return items.map(
+    ({ preview, original, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>
+    `
+  );
+}
+function renderItems(items) {
+  const markup = imageItems(items).join('');
+  listEl.insertAdjacentHTML('afterbegin', markup);
+}
+renderItems(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
