b7315e2bff9d5bd0f06e9187b9fcda4c9d404616
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-11-07 18:41:32 +0200

    3 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,7 +9,20 @@ const player = new Player(iframe, {
 
 player.on('timeupdate', throttle(onTimeupdate, 1000));
 
-player.setCurrentTime(loadToLS('videoplayer-current-time'));
+player
+  .setCurrentTime(loadToLS('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
 
 function onTimeupdate(data) {
   saveToLS('videoplayer-current-time', data.seconds);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
+const refs = {
+  formElem: document.querySelector('.feedback-form'),
+};
 
+refs.formElem.addEventListener('input', throttle(onFormElemInput, 500));
+refs.formElem.addEventListener('submit', onFormElemSubmit);
+
+loadPage();
+
+function onFormElemSubmit(e) {
+  e.preventDefault();
+
+  const data = loadToLS('feedback-form-state');
+  localStorage.removeItem('feedback-form-state');
+  e.target.reset();
+  console.log(data);
+}
+
+function onFormElemInput(e) {
+  const userData = {};
+  const formData = new FormData(refs.formElem);
+  formData.forEach((value, key) => {
+    userData[key] = value;
+  });
+  saveToLS('feedback-form-state', userData);
+}
+
+function loadPage() {
+  const data = loadToLS('feedback-form-state');
+  console.log(data);
+  for (const key of Object.keys(data)) {
+    refs.formElem.elements[key].value = data[key];
+  }
+}
+
+function saveToLS(key, value) {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+function loadToLS(key) {
+  try {
+    return JSON.parse(localStorage.getItem(key)) || {};
+  } catch (error) {
+    console.log(error.message);
+    return localStorage.getItem(key);
+  }
+}
