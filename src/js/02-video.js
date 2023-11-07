a8ff56d9b64d9c13a6493d7479242e10011e2c38
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-11-07 17:05:55 +0200

    two task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe, {
+  id: 19231868,
+  width: 640,
+});
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+player.setCurrentTime(loadToLS('videoplayer-current-time'));
+
+function onTimeupdate(data) {
+  saveToLS('videoplayer-current-time', data.seconds);
+}
+function saveToLS(key, value) {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+function loadToLS(key) {
+  try {
+    return JSON.parse(localStorage.getItem(key)) || {};
+  } catch (error) {
+    console.log(error.message);
+    return localStorage.getItem(key);
+  }
+}
